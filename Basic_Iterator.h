062865181df2079ffc3a7d93e01bc286fad6d92c
skyloved <w2783858980@outlook.com>
2024-10-16 18:35:00 +0800

    实现 BasicIterator 模板类及其基本功能
    添加了 `#pragma once` 和 include 防止头文件重复包含。
    引入了 `<iterator>` 头文件。
    定义了模板类 `BasicIterator`，实现了基本的迭代器功能。
    添加了构造函数、虚析构函数、解引用操作符、指针访问操作符、
    前置和后置递增操作符、赋值操作符、相等和不相等比较操作符、
    大小比较操作符、随机访问操作符等成员函数。
    添加了私有成员变量 `T* ptr` 用于存储指针。

---
+++ Basic_Iterator.h
@@ -0,0 +1,88 @@
+#pragma once
+#ifndef BASIC_ITERATOR_H
+#define BASIC_ITERATOR_H
+
+#include <iterator>
+
+template<typename T>
+class BasicIterator
+{
+public:
+	// 构造函数
+	BasicIterator() :ptr(nullptr) {};
+	BasicIterator(T* ptr = nullptr) : ptr(ptr) {}
+
+	// 虚析构函数
+	virtual ~BasicIterator() {}
+
+	// 解引用操作符
+	virtual T& operator*() const { return *ptr; }
+
+	// 指针访问操作符
+	virtual T* operator->() const { return ptr; }
+
+	// 前置递增操作符
+	virtual BasicIterator& operator++() {
+		++ptr;
+		return *this;
+	}
+
+	// 后置递增操作符
+	virtual BasicIterator operator++(int) {
+		BasicIterator temp = *this;
+		++(*this);
+		return temp;
+	}
+	virtual BasicIterator& operator=(const BasicIterator& other) {
+		if (this->ptr != other.ptr)
+		{
+			this->ptr = other.ptr;
+		}
+		return *this;
+	}
+
+	// 相等比较操作符
+	virtual bool operator==(const BasicIterator& other) const {
+		return ptr == other.ptr;
+	}
+
+	// 不相等比较操作符
+	virtual bool operator!=(const BasicIterator& other) const {
+		return ptr != other.ptr;
+	}
+
+	// 小于比较操作符
+	virtual bool operator<(const BasicIterator& other) const {
+		return ptr < other.ptr;
+	}
+
+	// 小于等于比较操作符
+	virtual bool operator<=(const BasicIterator& other) const {
+		return ptr <= other.ptr;
+	}
+
+	// 大于比较操作符
+	virtual bool operator>(const BasicIterator& other) const {
+		return ptr > other.ptr;
+	}
+
+	// 大于等于比较操作符
+	virtual bool operator>=(const BasicIterator& other) const {
+		return ptr >= other.ptr;
+	}
+
+	// 返回常量引用随机访问符
+	virtual const T& operator[](size_t index) const {
+		return ptr[index];
+	}
+
+	// 返回临时变量引用随机访问符
+	virtual T operator[](size_t index) {
+		return ptr[index];
+	}
+
+private:
+	T* ptr;
+};
+
+#endif // BASIC_ITERATOR_H
